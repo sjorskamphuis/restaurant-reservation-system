098db9412999f73507a29cf087f24d3036e8699f
Sjors Kamphuis <sjorskamphuis99@gmail.com>
2017-07-02 12:36:06 +0200

    updated seeder, added applications

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
          $this->call(UsersSeeder::class);
          $this->call(RolesSeeder::class);
+         $this->call(ApplicationsSeeder::class);
          $this->call(ApplicationUserRolesSeeder::class);
     }
 }
